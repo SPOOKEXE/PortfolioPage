b5388d7966b21e94fe95ba31b07e4ba037ffe7e0
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2022-07-23 13:55:03 +0930

    cleanup

--- portfolio_page/__init__.py
+++ portfolio_page/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/") # webpage_url/...
 def home():
-	return render_template("index.html")
+	return render_template("home.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
